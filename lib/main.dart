342e019f038018d2e3fdea32d7db9b10a8d70ac8
Mohamed-Abdel-Rahem <mohammedabdelrahem202123067@gmail.com>
2025-07-24 14:46:12 +0300

    add DevicePreview

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,31 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:subpay/screens/homePage.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  try {
+    await Firebase.initializeApp();
+    runApp(
+      DevicePreview(
+        enabled: true,
+        tools: const [...DevicePreview.defaultTools],
+        builder: (context) => const SubPayApp(),
+      ),
+    );
+  } catch (e) {
+    debugPrint('Firebase initialization error: $e');
+  }
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class SubPayApp extends StatelessWidget {
+  const SubPayApp({super.key});
 
   @override
   Widget build(BuildContext context) {
